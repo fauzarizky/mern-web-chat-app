1877ffd064cc2da06cb2ee942344940199742eb6
fauzarizky <fauzasmg@gmail.com>
2024-02-16 11:12:47 +0700

    First part completed - Backend

---
+++ backend/controllers/message.js
@@ -0,0 +1,71 @@
+import Conversation from "../models/conversation.js";
+import Message from "../models/message.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: reciverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, reciverId], // get the conversation between sender and reciver
+      },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, reciverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      reciverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage);
+    }
+
+    // await conversation.save();
+    // await newMessage.save();
+
+    // This will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error);
+    res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userTochatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, userTochatId],
+      },
+    }).populate("messages"); // Not reference but actual messages
+
+    if (!conversation) return res.sendStatus(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error);
+    res.status(500).json({
+      success: false,
+      error: "Internal server error",
+    });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.js";
+import messageRoutes from "./routes/message.js";
+import userRoutes from "./routes/user.js";
+
+
+import { connectToDB } from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
+app.use(cookieParser()); // to get the cookie
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectToDB();
+  console.log(`Server running on port ${PORT}`);
+});
