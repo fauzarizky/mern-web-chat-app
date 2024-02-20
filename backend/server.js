06b84321e0dc861d849c15613e063241b5f95841
fauzarizky <fauzasmg@gmail.com>
2024-02-20 14:29:18 +0700

    Deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,18 +12,23 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
 app.use(cookieParser()); // to get the cookie
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => { // any route except the ones above
+  // run the frontend from the server
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToDB();
